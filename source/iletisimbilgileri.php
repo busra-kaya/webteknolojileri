7f6d15d2e2c73eeb3c67994761bb9e4760dc2a94
busra-kaya <82663662+busra-kaya@users.noreply.github.com>
2021-05-23 13:39:32 +0300

    Add files via upload

---
+++ source/iletisimbilgileri.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html>
+   <body> 
+    <?php 
+
+    echo "  |AD-SOYAD : ";echo $_POST["name" ];   echo " | ";
+    echo "  |DOĞUM TARİHİ : ";echo $_POST["bday" ];    echo " | ";
+    echo "  |TELEFON NUMARASI : ";echo $_POST["tel"];    echo " | ";
+    echo "  |YAŞADIĞI ŞEHİR : ";echo $_POST["sehir"];    echo " | ";
+    echo "  |CİNSİYET : ";echo $_POST["cinsiyet"];    echo " | ";
+    echo "  |E-MAİL ADRESİ : ";echo $_POST["email"];    echo " | ";
+    echo "  |MESAJ : ";echo $_POST["message"];   echo " | ";
+    echo "  |YILDIZ : ";echo $_POST["yildiz"];    echo " | ";
+    echo "  |KARAR : ";echo $_POST["karar"];    echo " | ";
+
+
+    ?> 
+   </body>
+</html>
